bd8f46328e9f8a3efd8edd5e0547544f2a75d4b7
AlexBigBrain <alexcusto301102@gmail.com>
2021-05-05 17:04:54 +0200

    changing database

--- ServerApp/db.js
+++ ServerApp/db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'userDBTMM',
-    database: 'DBTMM',
+    host: 'esilxl0nthgloe1y.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    user: 'e6vernyqhz5glc54',
+    database: 'rf7xurjfj41nl2j2',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    password: 'userDBTMM.Progetto01'
+    password: 'veqwsfk4xm9hph69'
+    //password: 'userDBTMM.Progetto01'
 });
 
 
