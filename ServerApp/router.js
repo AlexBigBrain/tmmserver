2221ee935462ac657866817118621cb8651e8c73
ebalo <e.balsamo000@gmail.com>
2021-05-06 18:06:17 +0200

    crypto update + authentication

--- ServerApp/router.js
+++ ServerApp/router.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const crypto = require('crypto')
 var cors = require('cors')
-const { SearchWords, IncreaseCounterWord, ParolaRequest, InsertWord } = require('./controllerdb');
+const { SearchWords, IncreaseCounterWord, ParolaRequest, InsertWord, checkUsr } = require('./controllerdb');
+
+function getAuthentication(req) {
+    let hash = crypto.createHmac(
+        'sha512',                                               // algorithm
+        "6XxjUNFQ9bdTAPmNhnzm29bUwTVngHx2tUKuxmXjwlIv2fCF76"    // salt
+        )
+    hash.update(req.query.psw)
+
+    const usr = req.query.user,
+        psw = hash.digest('hex');
+    
+    return {usr, psw}
+}
 
 const router = express.Router();
 
 router.get('', async (req, res) => {
     const result = await ParolaRequest(req.query.word.toUpperCase());
     if (result.length === 0) {
+        // insert requested word
         res.json({
             "data": result,
             "status": 'Not Found'
@@ -20,18 +35,32 @@ router.get('', async (req, res) => {
 })
 
 router.post('/add', async (req, res) => {
-    const result = await InsertWord(req.query.word.toUpperCase(), req.query.lang.toUpperCase());
-    console.log(result);
+    const {usr, psw} = getAuthentication(req);
+
+    if(await checkUsr(usr, psw)) {
+        const result = await InsertWord(req.query.word.toUpperCase(), req.query.lang.toUpperCase());
+        console.log(result);
+        res.json({
+            status: (result > 0)
+        });
+    }
     res.json({
-        status: (result > 0)
-    });
+        error: "Authentication required"
+    })
 });
 
 router.patch('/patch', async (req, res) => {
-    const result = await IncreaseCounterWord(req.query.word.toUpperCase());
+    const {usr, psw} = getAuthentication();
+
+    if(await checkUsr(usr, psw)) {
+        const result = await IncreaseCounterWord(req.query.word.toUpperCase());
+        res.json({
+            status: (result > 0)
+        });
+    }
     res.json({
-        status: (result > 0)
-    });
+        error: "Authentication required"
+    })
 });
 
 router.get('/search', async (req, res) => {
